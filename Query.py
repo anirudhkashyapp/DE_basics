3331f123441170033ab21ee0cdf3370b1c758bfc
Anirudh <anisureshkashyap@gmail.com>
2026-04-25 15:26:16 +0530

    practice1

--- Query.py
+++ Query.py
@@ -2,15 +2,14 @@ import pandas as pd
 from sqlalchemy import create_engine
 engine = create_engine("sqlite:///weather.db")
 
-df_temp34 = pd.read_sql("""
+df_temp1 = pd.read_sql("""
     SELECT 
     w.city, 
-    ROUND(AVG(w.temp_max), 2) as avg_max
+    ROUND(AVG(w.temp_min), 2) as temp_min
 FROM weather w
 JOIN cities c ON w.city = c.city
 GROUP BY w.city
-HAVING AVG(w.temp_max) > 34
-ORDER BY avg_max DESC
+ORDER BY temp_min ASC
 """,engine)
-print("--- temperature greater than 34 ---")
-print(df_temp34)
+print("--- minimum average temperature ---")
+print(df_temp1)
