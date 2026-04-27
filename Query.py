aae341e9748965b485752b989020baddb8073d9f
Anirudh <anisureshkashyap@gmail.com>
2026-04-27 10:32:39 +0530

    practice4

--- Query.py
+++ Query.py
@@ -2,11 +2,12 @@ import pandas as pd
 from sqlalchemy import create_engine
 engine = create_engine("sqlite:///weather.db")
 
-df_temp3 = pd.read_sql("""
-  SELECT w.city, w.date, MAX(w.temp_max) as hottest_day
+df_temp4 = pd.read_sql("""
+  SELECT w.city, ROUND(SUM(w.precipitation),2) as total_rain
 FROM weather w
 JOIN cities c ON w.city = c.city
 GROUP BY w.city
-ORDER BY hottest_day DESC
+HAVING total_rain>10
+ORDER BY total_rain DESC
  """, engine)
-print(df_temp3)
+print(df_temp4)
